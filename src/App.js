3e81025bb1ff8d503fdd0e35d06df18fa71e06a4
Raktim02 <raktimproloy@gmail.com>
2021-06-16 16:31:45 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-
+import Login from './Components/Login/Login'
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      
-=======
     <div className="App">
-      <h1>Hello World!</h1>
->>>>>>> 7cd7a0ff2c9140247c1ac3ad7141c01498cc699a
+      <Login/>
     </div>
   );
 };
